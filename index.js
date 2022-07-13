712196cc56e03efb45f9e920020d10cb845c5ef2
vatovoss <evaristovoss13@gmail.com>
2022-07-12 23:06:13 -0300

    arrays

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-
-function calculoTotal (precioProducto, unidadesProducto) {
-    
-    let total = (precioProducto * unidadesProducto)
-    let final = total + envio;
-    return (final + envio) ;
+class Producto {
+    constructor(nombre,codigo,precio) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+    }
 }
 
-const envio = 150;
 
-let producto = parseFloat(prompt("Indicar el precio del producto:"));
-let unidades = parseInt(prompt("Indicar la cantidad de unidades:"));
+const lacteos = [];
+const reposteria = [];
 
-let precioFinal = calculoTotal (producto, unidades);
-alert("El precio final de los productos incluido el envio es $" + precioFinal);
+
+//lacteos
+lacteos.push(new Producto("Queso La Paulina 1kg", 1, 550));
+//reposteria
+reposteria.push(new Producto("Harina Industrial", 2, 1500));
+
+
+for (const lacteos of lacteos) {
+    alert("CODIGO (" + lacteos.codigo + ") - " + lacteos.nombre);
+}
+for (const reposteria of reposteria) {
+    alert("CODIGO (" + reposteria.codigo + ") - " + reposteria.nombre);
+}
+
+if (productoElegido <= 0) {
+    alert("Ingresa un codigo valido");
+} else if (productoElegido <=1) {
+    alert("Seleccionaste " +  lacteosFind.nombre + "\nToca aceptar para agregarlo a tu carrito"); 
+} else if (productoElegido <=2) {
+    alert("Seleccionaste " + reposteriaFind.nombre + "\nToca aceptar para agregarlo a tu carrito");
+} 
